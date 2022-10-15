9ccfe9bf424a6a0d755b60db3ab85701743e9573
OlegVazhinsky <oleg_vazhinsky92@mail.ru>
2022-10-15 16:52:03 +0700

    Выполнил задание 6

---
+++ Task6/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Ведите число - ");
+int userNumber = int.Parse(Console.ReadLine());
+if (userNumber % 2 == 0) Console.WriteLine($"Число {userNumber} четное");
+else Console.WriteLine($"Число {userNumber} не четное");
