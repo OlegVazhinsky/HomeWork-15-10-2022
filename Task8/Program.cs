c64f408a1c85b410895418438d53a91d427f0a44
OlegVazhinsky <oleg_vazhinsky92@mail.ru>
2022-10-15 17:00:47 +0700

    Выполнено задание 8

---
+++ Task8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ведите число - ");
+int userNumber = int.Parse(Console.ReadLine());
+if (userNumber >= 0) {
+    for (int i = 2; i <= userNumber; i = i + 2) {
+        Console.Write($"{i} ");
+    }
+}
+else {
+    for (int i = userNumber; i < 0; i = i + 2) {
+        Console.Write($"{i} ");
+    }
+}
