8b36175b892ecb4edae977b486a4323cbcfbfdf3
OlegVazhinsky <oleg_vazhinsky92@mail.ru>
2022-10-15 15:48:02 +0700

    Сделал задание 2

---
+++ Task2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Ведите первое число - ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.Write("Ведите второе число - ");
+int secondNumber = int.Parse(Console.ReadLine());
+if (firstNumber > secondNumber) Console.WriteLine($"a = {firstNumber}; b = {secondNumber} -> max = {firstNumber}");
+else Console.WriteLine($"a = {firstNumber}; b = {secondNumber} -> max = {secondNumber}");
