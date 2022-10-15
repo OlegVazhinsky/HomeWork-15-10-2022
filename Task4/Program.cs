8f40b04446209a7f98d23e3535151067f70f2645
OlegVazhinsky <oleg_vazhinsky92@mail.ru>
2022-10-15 16:46:49 +0700

    Выполнено задание 4

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Ведите первое число - ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.Write("Ведите второе число - ");
+int secondNumber = int.Parse(Console.ReadLine());
+Console.Write("Ведите третье число - ");
+int thirdNumber = int.Parse(Console.ReadLine());
+
+
+if (firstNumber > secondNumber) {
+    if (firstNumber > thirdNumber) Console.WriteLine($"a = {firstNumber}; b = {secondNumber}; c = {thirdNumber} -> max = {firstNumber}");
+    else Console.WriteLine($"a = {firstNumber}; b = {secondNumber}; c = {thirdNumber} -> max = {thirdNumber}");
+}
+else {
+    if (secondNumber > thirdNumber) Console.WriteLine($"a = {firstNumber}; b = {secondNumber}; c = {thirdNumber} -> max = {secondNumber}");
+    else Console.WriteLine($"a = {firstNumber}; b = {secondNumber}; c = {thirdNumber} -> max = {thirdNumber}");
+}
